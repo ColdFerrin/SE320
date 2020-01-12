dceb322302c13a1dc60f721d803d8e8428134ee0
Ferrin Katz <ferrinkatz@ferrinkatz.com>
2020-01-12 13:06:56 -0700

    use f2c1

---
+++ hw1/f2c1/f2c1.adb
@@ -0,0 +1,13 @@
+with Ada.Text_IO, Ada.Float_Text_IO;
+
+procedure f2c1 is
+  f,
+  c: Float;
+begin
+  Ada.Text_IO.Put_Line ("How many items?");
+  Ada.Text_IO.Flush;
+  f := Float'Value (Ada.Text_IO.Get);
+  c := (((f - 32.0) * 5.0) / 9.0);
+  Ada.Text_IO.Put_Line ("The temperature " Float'Image (c)  " in Fahrenheit,
+  is" Integer'Image (f) " in Celsius.");
+end f2c1;
