539ad574f5ddbd4e454e53962969807fb612ad07
u_temp <e_temp>
2020-04-20 23:29:27 -0700

    update hw5

--- hw5/array_list_package.adb
+++ hw5/array_list_package.adb
@@ -2,7 +2,14 @@ with Ada.Text_IO, Ada.Integer_Text_IO;
 
 package body Array_List_Package is
    
-   procedure Init(List: in out Array_List_Type; Size: in Integer) is
+   procedure Init(List: in out Array_List_Type; 
+                  Size: in Integer) is
+   begin
+      List.Data := new Data_Array_Type(1 .. Size);
+   end Init;
+
+   procedure Init(List: in Array_List_Access_Type; 
+                  Size: in Integer) is
    begin
       List.Data := new Data_Array_Type(1 .. Size);
    end Init;
@@ -19,8 +26,32 @@ package body Array_List_Package is
          Success := False;
       end if;
    end Add;
+
+   procedure Add(List: in Array_List_Access_Type;
+                 Value: in Data_Type;
+                 Success: out Boolean) is
+   begin
+      if (List.Length < List.Data'Last) then
+         List.Length := List.Length + 1;
+         List.Data(List.Length) := Value;
+         Success := True;
+      else
+         Success := False;
+      end if;
+   end Add;
    
    function Find(List: in Array_List_Type;
+                 Value: in Data_Type) return Index_Type is
+   begin
+      for Index in 1 .. List.Length loop
+         if List.Data(Index) = Value then
+            return Index_Type(Index);
+         end if;
+      end loop;
+      return 0;
+   end Find;
+
+   function Find(List: in Array_List_Access_Type;
                  Value: in Data_Type) return Index_Type is
    begin
       for Index in 1 .. List.Length loop
@@ -45,6 +76,21 @@ package body Array_List_Package is
          Success := False;
       end if;
    end Remove;
+
+   procedure Remove(List: in Array_List_Access_Type;
+                    Index: in Index_Type;
+                    Success: out Boolean) is
+   begin
+      if Index > 0 and Integer(Index) <= List.Length then
+         List.Length := List.Length -1;
+         for I in Integer(Index) .. List.Length loop
+            list.Data(I) := List.Data(I + 1);
+         end loop;
+         Success := True;
+      else
+         Success := False;
+      end if;
+   end Remove;
    
    procedure Remove (List    : in out Array_List_Type;
                      Value   : in     Data_Type;
@@ -53,9 +99,32 @@ package body Array_List_Package is
       Remove(List, Find(List, Value), Success);
    end Remove;
 
+   procedure Remove (List: in Array_List_Access_Type;
+                     Value: in Data_Type;
+                     Success: out Boolean) is
+   begin
+      Remove(List, Find(List, Value), Success);
+   end Remove;
+
 
    procedure Copy(Source: in Array_List_Type;
                   Destination: in out Array_List_Type) is
+      Copy_Length: Integer := 0;
+   begin
+      
+      if Source.Length > Destination.Data'Last then
+         Copy_Length := Destination.Data'Last;
+      else
+         Copy_Length := Source.Length;
+      end if;
+      for I in 1 .. Copy_Length loop
+         Destination.Data(I) := Source.Data(I);
+      end loop;
+      Destination.Length := Copy_Length;
+   end Copy;
+
+   procedure Copy(Source: in Array_List_Access_Type;
+                  Destination: in  Array_List_Access_Type) is
       Copy_Length: Integer := 0;
    begin
       if Source.Length > Destination.Data'Last then
@@ -83,5 +152,32 @@ package body Array_List_Package is
       end loop;
       Ada.Text_IO.New_Line;
    end Put_Line;
+
+   procedure Put_Line(List: in Array_List_Access_Type) is
+   begin
+      for I in 1 .. List.Length loop
+         Ada.Integer_Text_IO.Put(Integer(List.Data(I)), 4);
+         if I < List.Length then
+            if I mod 10 = 0 then
+               Ada.Text_IO.Put_Line(",");
+            else
+               Ada.Text_IO.Put(", ");
+            end if;
+         end if;
+      end loop;
+      Ada.Text_IO.New_Line;
+   end Put_Line;
+
+   procedure Create(Size: in Integer; Ptr: in out Array_List_Access_Type) is
+   begin
+      Ptr := new Array_List_Type;
+      Init(Ptr, Size);
+   end Create;
+
+   procedure Create(Size: in Integer; Ptr: in out Array_List_Type) is
+   begin
+      Ptr := new Array_List_Type;
+      Init(Ptr, Size);
+   end Create;
 
 end Array_List_Package;
--- hw5/main.adb
+++ hw5/main.adb
@@ -3,9 +3,12 @@ with Array_List_Package;
 procedure Main is
    List: Array_List_Package.Array_List_Type;
    Copy: Array_List_Package.Array_List_Type;
+   Ptr: Array_List_Package.Array_List_Access_Type;
+   Ptr_Cpy: Array_List_Package.Array_List_Access_Type;
    Result: Boolean;
 begin
    Array_List_Package.Init(List, 10);
+   Array_List_Package.Init(Copy, 10);
    Array_List_Package.Add(List, 5, Result);
    Array_List_Package.Add(List, 7, Result);
    Array_List_Package.Add(List, 2, Result);
@@ -21,6 +24,28 @@ begin
    Array_List_Package.Remove(List, Array_List_Package.Index_Type(7), Result);
    Array_List_Package.Remove(List, Array_List_Package.Data_Type(7), Result);
    Array_List_Package.Put_Line(List);
+   Array_List_Package.Copy(List, Copy);
+   Array_List_Package.Put_Line(Copy);
+   Array_List_Package.Create(10, Ptr);
+   Array_List_Package.Add(Ptr, 5, Result);
+   Array_List_Package.Add(Ptr, 7, Result);
+   Array_List_Package.Add(Ptr, 2, Result);
+   Array_List_Package.Add(Ptr, 6, Result);
+   Array_List_Package.Add(Ptr, 1, Result);
+   Array_List_Package.Add(Ptr, 17, Result);
+   Array_List_Package.Add(Ptr, 3, Result);
+   Array_List_Package.Add(Ptr, 9, Result);
+   Array_List_Package.Add(Ptr, 57, Result);
+   Array_List_Package.Add(Ptr, 17, Result);
+   Array_List_Package.Add(Ptr, 8, Result);
+   Array_List_Package.Put_Line(Ptr);
+   Array_List_Package.Remove(Ptr, Array_List_Package.Index_Type(7), Result);
+   Array_List_Package.Remove(Ptr, Array_List_Package.Data_Type(7), Result);
+   Array_List_Package.Put_Line(Ptr);
+   Array_List_Package.Create(10, Ptr_Cpy);
+   Array_List_Package.Copy(Ptr, Ptr_Cpy);
+   Array_List_Package.Put_Line(Ptr_Cpy);
+
 end Main;
 
    
